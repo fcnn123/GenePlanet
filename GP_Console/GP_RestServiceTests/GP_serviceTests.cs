63438f274c483e6336dffb9b9ccfb3feb63886f0
Tadej Bracun <tadej.bracun@gmail.com>
2019-12-15 21:36:57 +0100

    Integration test

---
+++ GP_Console/GP_RestServiceTests/GP_serviceTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GP_RestService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Web;
+using GP_Dal.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace GP_RestService.Tests
+{
+    [TestClass()]
+    public class GP_serviceTests
+    {
+        [TestMethod()]
+        public async Task GetAggDataTest()
+        {
+            Uri baseAddress = new Uri("http://localhost:5080/");
+            string username = "7YQRY6";
+            string mode = "all";
+
+            using (WebServiceHost host = new WebServiceHost(typeof(GP_service), baseAddress))
+            {
+                host.Open();
+
+                try
+                {
+                    using (var client = new HttpClient())
+                    {
+                        var response = await client.GetAsync(string.Format("{0}AggData/{1}/{2}", baseAddress, username, mode));
+                        response.EnsureSuccessStatusCode();
+
+                        var jsonString = response.Content.ReadAsStringAsync();
+                        AggData aggData = JsonConvert.DeserializeObject<AggData>(jsonString.Result);
+
+                        Assert.IsNotNull(aggData);
+                    }
+
+                }
+                catch (HttpRequestException eReq)
+                {
+                    Assert.Fail(eReq.Message);
+                }
+                catch (Exception e)
+                {
+                    Assert.Fail(e.Message);
+                }
+
+                host.Close();
+            }
+        }
+    }
+}
