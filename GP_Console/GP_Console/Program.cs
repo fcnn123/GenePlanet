06cb78aa77f30cf72cd42ee369738baf15387831
Tadej Bracun <tadej.bracun@gmail.com>
2019-12-15 19:23:05 +0100

    GetOnlineData

--- GP_Console/GP_Console/Program.cs
+++ GP_Console/GP_Console/Program.cs
@@ -1,7 +1,10 @@
-﻿using GP_RestService;
+﻿using GP_Dal.Models;
+using GP_RestService;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.ServiceModel.Web;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,10 +26,54 @@ namespace GP_Console
                 Console.WriteLine("Possible values for parameter {0} are \"all\",\"min\" or \"max\"", "{param}");
                 Console.WriteLine("Press <Enter> to stop the service.");
 
+                GetOnlineData("7YQRY6", "localhost:8083");
+
                 Console.ReadLine();
 
                 host.Close();
             }
+        }
+
+        public static async Task GetOnlineData(string username, string shimmer_host)
+        {
+            Console.WriteLine("");
+
+            string[] shims = new string[] { "fitbit", "ihealth" };
+
+            MHealth_body_weight mHealth = null;
+            using (var client = new HttpClient())
+            {
+                DateTime t = DateTime.Today;
+                foreach (var shim in shims)
+                {
+                    try
+                    {
+                        var response = await client.GetAsync(string.Format("http://{0}/data/{1}/body_weight?username={2}&dateStart={3}&dateEnd={3}&normalize=true", shimmer_host, shim, username, t.ToString("yyy-MM-dd")));
+                        response.EnsureSuccessStatusCode();
+
+                        var jsonString = response.Content.ReadAsStringAsync();
+                        mHealth = JsonConvert.DeserializeObject<MHealth_body_weight>(jsonString.Result);
+
+                        foreach (var item in mHealth.body)
+                        {
+                            float weight = 0;
+                            if (item.body.body_weight.unit == "kg")
+                                weight = item.body.body_weight.value;
+                            else
+                                weight = item.body.body_weight.value / 2.2046F;
+                            Console.WriteLine(string.Format("Shim \"{0}\" ; Weight: {1}", shim, weight));
+                        }                        
+                    }
+                    catch (HttpRequestException eReq)
+                    {
+                        Console.WriteLine(string.Format("Error on request call for shim \"{0}\": {1}", shim, eReq.Message));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(string.Format("Error on sync for shim \"{0}\": {1}", shim, e.Message));
+                    }
+                }
+            }            
         }
     }
 }
---
+++ GP_Console/GP_Dal/Models/MHealth_body_weight.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP_Dal.Models
+{
+    public class MHealth_body_weight
+    {
+        public string shim { get; set; }
+        public int timeStamp { get; set; }
+        public Body[] body { get; set; }
+    }
+
+    public class Body
+    {
+        public Header header { get; set; }
+        public Body1 body { get; set; }
+    }
+
+    public class Header
+    {
+        public string id { get; set; }
+        public DateTime creation_date_time { get; set; }
+        public Acquisition_Provenance acquisition_provenance { get; set; }
+        public Schema_Id schema_id { get; set; }
+    }
+
+    public class Acquisition_Provenance
+    {
+        public string source_name { get; set; }
+        public long external_id { get; set; }
+    }
+
+    public class Schema_Id
+    {
+        public string _namespace { get; set; }
+        public string name { get; set; }
+        public string version { get; set; }
+    }
+
+    public class Body1
+    {
+        public Effective_Time_Frame effective_time_frame { get; set; }
+        public Body_Weight body_weight { get; set; }
+    }
+
+    public class Effective_Time_Frame
+    {
+        public DateTime date_time { get; set; }
+    }
+
+    public class Body_Weight
+    {
+        public string unit { get; set; }
+        public float value { get; set; }
+    }
+
+}
